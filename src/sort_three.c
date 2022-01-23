20c6b046f30bbd0e490b32a91c864e5c0dbd0ca0
Анастасия Мартынова <anastasiamartynova@MacBook-Pro-Anastasia.local>
2022-01-23 11:52:49 +0300

    3

--- src/sort_three.c
+++ src/sort_three.c
@@ -22,16 +22,25 @@ int	check_ifsorted(t_list	**stack)
 
 void    sort_three(t_list **stack)
 {
-    t_list	*second;
-	t_list	*third;
+    t_list	*third;
+	t_list	*second;
 
-	second = (*stack)->next;
+    second = (*stack)->next;
 	third = second->next;
-	if ((second->content > third->content) || ((*stack)->content < third->content))
-		swap(stack);
-	if ((*stack)->content > second->content || (*stack)->content < third->content)
-	 	swap(stack);
-	if ((*stack)->content > second->content || (*stack)->content > third->content)
-	 	rotate(stack);
-	return (reverse_rotate(stack));
+    if (third->content > (*stack)->content && second->content < (*stack)->content)
+        swap(stack);
+    else if (third->content < second->content && (*stack)->content > second->content)
+    {
+        swap(stack);
+        reverse_rotate(stack);
+    }
+    else if (third->content < (*stack)->content && second->content < third->content)
+        rotate(stack);
+    else if (second->content > third->content && (*stack)->content < third->content)
+    {
+        swap(stack);
+        rotate(stack);
+    }
+    else if ((*stack)->content < second->content && third->content < (*stack)->content)
+        reverse_rotate(stack);
 }
